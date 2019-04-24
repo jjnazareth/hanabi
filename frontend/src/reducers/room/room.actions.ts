e94fa8d84f4c8c6fa27b2764b29e0647031f10d6
nikeshnazareth <business@nikeshnazareth.com>
2019-04-24 14:10:15 +1000

    Update initialisePlayers to a more complex state
    We are using initialisePlayers to test the relationship between
    the redux store and the components. This commit updates the
    init action to add additional players and set the currentPlayer
    and turnIdx

--- frontend/src/reducers/room/room.actions.ts
+++ frontend/src/reducers/room/room.actions.ts
@@ -1,10 +1,34 @@
 import { Dispatch } from 'redux'
-import { AddPlayer, RoomActionNames } from './room.actions.type'
+import { AddPlayer, RoomActionNames, SetCurrentPlayer, SetTurnIdx } from './room.actions.type'
 
 
-export const initialisePlayers = () => (dispatch: Dispatch<AddPlayer>) => {
+export const initialisePlayers = () => (dispatch: Dispatch<AddPlayer | SetTurnIdx | SetCurrentPlayer >) => {
     dispatch({
         type: RoomActionNames.ADD_PLAYER,
         name: 'Jivraj'
+    })
+    dispatch({
+        type: RoomActionNames.ADD_PLAYER,
+        name: 'Nikesh'
+    })
+    dispatch({
+        type: RoomActionNames.ADD_PLAYER,
+        name: 'Shanta'
+    })
+    dispatch({
+        type: RoomActionNames.ADD_PLAYER,
+        name: 'Nitin'
+    })
+    dispatch({
+        type: RoomActionNames.ADD_PLAYER,
+        name: 'Mikey'
+    })
+    dispatch({
+        type: RoomActionNames.SET_TURN_IDX,
+        idx: 2
+    })
+    dispatch({
+        type: RoomActionNames.SET_CURRENT_PLAYER,
+        idx: 0
     })
 }
