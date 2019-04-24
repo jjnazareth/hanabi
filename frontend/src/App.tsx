20b3cd55eeca78cb0ab03705d8ccc1d9cc223d2a
nikeshnazareth <business@nikeshnazareth.com>
2019-04-24 13:07:37 +1000

    Simplify main App component
    create-react-app creates a default App component. This commit
    replaces that component with a placeholder Container component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,13 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import logo from './logo.svg'
-import './App.css'
 import store from './store'
+import Container from './components/Container'
 
 class App extends Component {
     render() {
         return (
             <Provider store={ store }>
-                <div className='App'>
-                    <header className='App-header'>
-                        <img src={ logo } className='App-logo' alt='logo' />
-                        <p>
-                            Edit <code>src/App.tsx</code> and save to reload.
-                        </p>
-                        <a
-                            className='App-link'
-                            href='https://reactjs.org'
-                            target='_blank'
-                            rel='noopener noreferrer'
-                        >
-                            Learn React
-                        </a>
-                    </header>
-                </div>
+                <Container></Container>
             </Provider>
         )
     }
---
+++ frontend/src/components/Container.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Container extends Component<{}> {
+    public render(): JSX.Element {
+        return (
+            <div>This is a react component</div>
+        )
+    }
+}
+
+export default Container
